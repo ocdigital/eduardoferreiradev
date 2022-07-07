b5fd68ec72890d391f32f7816aff32f9b6b8d15f
Eduardo Ferreira <eduardo@ocdigital.com.br>
2022-07-06 21:36:18 -0300

    Fix Missing or insufficient Content Security Policy Header

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
+        \Spatie\Csp\AddCspHeaders::class,
     ];
 
     /**
